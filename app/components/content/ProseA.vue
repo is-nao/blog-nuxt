0aa11ec362ea8829fd9ca379c2ec7f4921f7a138
nao <cookie.nao27@gmail.com>
2025-09-13 23:40:40 +0900

    remove external icon

--- app/components/content/ProseA.vue
+++ app/components/content/ProseA.vue
@@ -7,7 +7,7 @@ const appConfig = useAppConfig()
 const ui = computed(() => tv({
   extend: tv({
     slots: { base: theme.base }
-  }), ...appConfig.ui?.prose?.a
+  })
 })())
 
 const props = withDefaults(defineProps<{
@@ -27,11 +27,7 @@ const target = computed(() => props.href.startsWith('http') ? '_blank' : props.t
 
 <template>
   <LinkCard v-if="props.card" :href="props.href" :target />
-  <ULink
-    v-else :href="props.href" :target
-    :class="ui.base({ class: props.class })" raw
-  >
+  <ULink v-else :href="props.href" :target :class="ui.base({ class: props.class })" raw>
     <slot />
-    <UIcon v-if="target === '_blank'" name="i-lucide-external-link" :class="ui.externalIcon()"/>
   </ULink>
 </template>
